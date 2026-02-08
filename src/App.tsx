4bd4d61f11772c1d657aa6cc4bc5d1c228298e4c
Amanvd2610 <amanchandra418@gmail.com>
2026-02-08 23:48:54 +0530

    updated another prob

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/night-of-88-premiere">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
